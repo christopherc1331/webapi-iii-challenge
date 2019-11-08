39d55a86f13a68501c0d1139c5b8a24846f56a97
Chris Cruz <christopherc1331@gmail.com>
2019-11-07 17:53:09 -0800

    Final changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require("dotenv").config();
+
 const server = require("./server.js");
 
-server.listen(4000, () => {
-    console.log("\nServer is listening at port 4000\n")
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\nServer is listening at port ${port}\n`);
 });
